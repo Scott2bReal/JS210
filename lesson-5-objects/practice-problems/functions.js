d045ce27c16ed4c4ea3a520599db42b9c98f0ce5
Scott Hoecker <hoeckers@gmail.com>
2022-08-25 11:54:03 -0500

    Finish pure functions practice problems

--- lesson-5-objects/practice-problems/functions.js
+++ lesson-5-objects/practice-problems/functions.js
@@ -19,3 +19,31 @@ foo(qux);
 1. The array qux is mutated on line 1 of the function body
 2. The popped value is logged to the console on line 2 of the function body
 */
+
+/*
+Which of the following functions are pure functions?
+*/
+
+function sum(a, b) {
+  console.log(a + b);
+  return a + b;
+}
+
+// Pure
+function sum(a, b) {
+  a + b;
+}
+
+// Pure
+function sum(a, b) {
+  return a + b;
+}
+
+function sum(a, b) {
+  return a + b + Math.random();
+}
+
+// Pure
+function sum(a, b) {
+  return 3.1415;
+}
